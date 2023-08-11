541bbb13ede2d334dd04c509ea527eac096561ee
Shreyasrana18 <srana1_be20@thapar.edu>
2023-08-11 15:06:55 +0530

    fixes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const errorHandler = require('./middleware/errorHandling');
 const connectDb = require('./config/dbConnection');
+const cors = require('cors');
 
 connectDb();
 
@@ -9,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/schools/", require("./routes/schoolRoutes")); 
 app.use("/api/auth/", require("./routes/loginregisRoutes"));
 app.use("/api/students/", require("./routes/studentRoutes"));
--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { schoolInformation, createStudent, createquizzes, clearquiz, getquiz, createShoppingList,getShoppingList,deleteShoppingList,updateShoppingList,deleteShoppingItems } = require('../controllers/schoolController');
 
+
 router.route('/student').get(schoolInformation);
 router.route('/student/:id').post(createStudent);
 router.route('/quizzes/:id').post(createquizzes).delete(clearquiz).get(getquiz);
